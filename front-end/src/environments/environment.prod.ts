96e38ba22371bf1df4cc085e26d8536ee505a033
Marco Serradas <marco.serradas@gmail.com>
2018-05-27 22:16:52 +0200

    Auto stash before merge of "master" and "origin/master"

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/cord-Angular4-phpSlim/api'
+  apiUrl: 'http://localhost/api/index.php'
 };
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: './php-api/index.php'
+  apiUrl: 'http://localhost/php-api'
 };
 
 /*
